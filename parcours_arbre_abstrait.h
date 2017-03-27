a9dd3231e36b72c98ee3e7d81a25556910707f6d
dkei15 <alex.taylor@etu.univ-amu.fr>
2017-03-27 16:00:04 +0200

    Add files via upload

---
+++ parcours_arbre_abstrait.h
@@ -0,0 +1,34 @@
+#ifndef __AFFICHE_ARBRE_ABSTRAIT__
+#define __AFFICHE_ARBRE_ABSTRAIT__
+
+#include "syntabs.h"
+void parcours_n_prog(n_prog *n);
+void parcours_l_instr(n_l_instr *n);
+void parcours_instr(n_instr *n);
+void parcours_instr_si(n_instr *n);
+void parcours_instr_tantque(n_instr *n);
+void parcours_instr_faire(n_instr *n);      /* MODIFIE POUR EVAL */
+//void parcours_instr_pour(n_instr *n);       /* MODIFIE POUR EVAL */
+void parcours_instr_affect(n_instr *n);
+void parcours_instr_appel(n_instr *n);
+void parcours_instr_retour(n_instr *n);
+void parcours_instr_ecrire(n_instr *n);
+void parcours_l_exp(n_l_exp *n);
+void parcours_exp(n_exp *n);
+void parcours_varExp(n_exp *n);
+void parcours_opExp(n_exp *n);
+void parcours_intExp(n_exp *n);
+void parcours_lireExp(n_exp *n);
+void parcours_appelExp(n_exp *n);
+void parcours_l_dec(n_l_dec *n);
+void parcours_dec(n_dec *n);
+void parcours_foncDec(n_dec *n);
+void parcours_varDec(n_dec *n);
+void parcours_tabDec(n_dec *n);
+void parcours_var(n_var *n);
+void parcours_var_simple(n_var *n);
+void parcours_var_indicee(n_var *n);
+void parcours_appel(n_appel *n);
+
+
+#endif
